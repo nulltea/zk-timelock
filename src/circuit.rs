2215bbec0f9c47b34b46dc3d435298a31eb4950e
Timofey Luin <timofey.luin@gmail.com>
2022-10-11 11:58:27 +0300

    Add benchmarks

--- src/circuit.rs
+++ src/circuit.rs
@@ -36,6 +36,8 @@ use sha2::Sha256;
 use crate::nonnative::*;
 use crate::{Randomness, Plaintext, Ciphertext, PublicKey, SecretKey, Parameters};
 
+const R_BYTES_SQUEEZE: usize = 16;
+
 pub struct Circuit<E: PairingEngine, P: Bls12Parameters<Fp = E::Fq>>
 {
     gid: E::Fqk,
@@ -78,10 +80,11 @@ impl<E: PairingEngine, P: Bls12Parameters<Fp = E::Fq>> Circuit<E, P>
         master: &PublicKey<E>,
         id: I,
         msg: &Plaintext<E::G1Projective>,
-        params: &Parameters<E::G1Projective>,
         rng: &mut R,
     ) -> anyhow::Result<Ciphertext<E::G1Projective>> {
-        let (_, _, ct) = Self::encrypt_inner(master, id, msg, params, rng)?;
+        let params = Parameters::<E::G1Projective>::default();
+
+        let (_, _, ct) = Self::encrypt_inner(master, id, msg, &params, rng)?;
         Ok(ct)
     }
 
@@ -108,7 +111,7 @@ impl<E: PairingEngine, P: Bls12Parameters<Fp = E::Fq>> Circuit<E, P>
             let mut sponge = PoseidonSponge::new(&params.poseidon);
             sponge.absorb(&sigma.0);
             sponge.absorb(&msg);
-            sponge.clone().squeeze_bytes(32)
+            sponge.clone().squeeze_bytes(R_BYTES_SQUEEZE)
         };
 
         // 4. Compute U = G^r
@@ -147,8 +150,9 @@ impl<E: PairingEngine, P: Bls12Parameters<Fp = E::Fq>> Circuit<E, P>
     pub fn decrypt(
         sk: &SecretKey<E>,
         ct: &Ciphertext<E::G1Projective>,
-        params: &Parameters<E::G1Projective>,
     ) -> anyhow::Result<Plaintext<E::G1Projective>> {
+        let params = Parameters::<E::G1Projective>::default();
+
         // 1. Compute sigma = V XOR H2(e(rP,private))
         let sigma = {
             let r_gid = E::pairing(ct.u.clone(), sk.clone());
@@ -176,7 +180,7 @@ impl<E: PairingEngine, P: Bls12Parameters<Fp = E::Fq>> Circuit<E, P>
             sponge.absorb(&msg);
             curve_scalar_mul_le(
                 E::G1Projective::prime_subgroup_generator(),
-                &sponge.squeeze_bytes(32)
+                &sponge.squeeze_bytes(R_BYTES_SQUEEZE)
             )
         };
         assert_eq!(ct.u, r_g);
@@ -199,7 +203,7 @@ impl<E: PairingEngine, P: Bls12Parameters<Fp = E::Fq>> Circuit<E, P>
             sponge.absorb(&sigma)?;
             sponge.absorb(&msg)?;
             sponge
-                .squeeze_bytes(32)?
+                .squeeze_bytes(R_BYTES_SQUEEZE)?
                 .into_iter().flat_map(|b| b.to_bits_le().unwrap()).collect::<Vec<_>>()
         };
 
@@ -346,6 +350,13 @@ impl<PC: ProjectiveCurve> NonnativeCircuit<PC>
         vec![]
     }
 
+    pub fn decrypt(
+        sk: &SecretKey<Bls12_381>,
+        ct: &Ciphertext<ark_bls12_381::G1Projective>,
+    ) -> anyhow::Result<Plaintext<ark_bls12_381::G1Projective>> {
+        Circuit::<Bls12_381, ark_bls12_381::Parameters>::decrypt(sk, ct)
+    }
+
     pub(crate) fn verify_encryption(
         &self,
         cs: ConstraintSystemRef<PC::BaseField>,
@@ -375,8 +386,8 @@ impl<PC: ProjectiveCurve> NonnativeCircuit<PC>
             sponge.absorb(&sigma.to_constraint_field().unwrap())?;
             sponge.absorb(&msg.to_constraint_field().unwrap())?;
             sponge
-                .squeeze_nonnative_field_elements::<ark_bls12_381::Fq>(1)
-                .and_then(|r| Ok(r.0[0].clone()))?.to_bits_le()?
+                .squeeze_bytes(R_BYTES_SQUEEZE)?
+                .into_iter().flat_map(|b| b.to_bits_le().unwrap()).collect::<Vec<_>>()
         };
 
         // 4. Compute U = G*r
@@ -497,7 +508,6 @@ mod tests {
         let bytes = [1, 2, 3];
         let msg = ark_bls12_381::Fq::from_random_bytes(&bytes).unwrap();
 
-        let params = Parameters::<ark_bls12_381::G1Projective>::default();
         let pk = {
             let bytes = hex::decode("8200fc249deb0148eb918d6e213980c5d01acd7fc251900d9260136da3b54836ce125172399ddc69c4e3e11429b62c11").unwrap();
             ark_bls12_381::G1Affine::deserialize_zk_crypto(&bytes).unwrap()
@@ -510,91 +520,14 @@ mod tests {
             &hash.finalize().to_vec()[0..32]
         };
 
-        let ct = TestCircuit::encrypt(&pk, id, &msg, &params, &mut rng).unwrap();
+        let ct = TestCircuit::encrypt(&pk, id, &msg, &mut rng).unwrap();
 
         let sk = {
             let bytes = hex::decode("a4721e6c3eafcd823f138cd29c6c82e8c5149101d0bb4bafddbac1c2d1fe3738895e4e21dd4b8b41bf007046440220910bb1cdb91f50a84a0d7f33ff2e8577aa62ac64b35a291a728a9db5ac91e06d1312b48a376138d77b4d6ad27c24221afe").unwrap();
             ark_bls12_381::G2Affine::deserialize_zk_crypto(&bytes).unwrap()
         };
 
-        let pt = TestCircuit::decrypt(&sk, &ct, &params).unwrap();
+        let pt = TestCircuit::decrypt(&sk, &ct).unwrap();
         // assert_eq!(pt, msg)
-    }
-
-    #[test]
-    fn test_circuit_377() {
-        type TestCircuit = Circuit::<Bls12_377, ark_bls12_377::Parameters>;
-        let mut rng = test_rng();
-        let bytes = [1, 2, 3];
-        let msg = ark_bls12_377::Fq::from_random_bytes(&bytes).unwrap();
-
-        let (master, priv_key) = {
-            let sk = ark_bls12_377::Fr::rand(&mut rng);
-            let mut master = {
-                let mut g = ark_bls12_377::G1Projective::prime_subgroup_generator();
-                g.mul_assign(sk.clone());
-                g
-            };
-            let msg_hash = ark_bls12_377::G2Projective::prime_subgroup_generator();
-            let signature = {
-                let mut h = msg_hash;
-                h.mul_assign(sk);
-                h
-            };
-            (master.into_affine(), signature.into_affine())
-        };
-
-        let params = Parameters::<ark_bls12_377::G1Projective>::default();
-
-        let round_number = 1000u64;
-        let id = {
-            let mut hash = sha2::Sha256::new();
-            hash.update(&round_number.to_be_bytes());
-            &hash.finalize().to_vec()[0..32]
-        };
-
-        let circuit = Circuit::<ark_bls12_377::Bls12_377, ark_bls12_377::Parameters>::new(
-            master.clone(),
-            &id,
-            msg.clone().into(),
-            &mut rng,
-        ).unwrap();
-
-        let (pk, _vk) = Groth16::<BW6_761>::setup(circuit, &mut rng).unwrap();
-
-        let circuit = Circuit::<ark_bls12_377::Bls12_377, ark_bls12_377::Parameters>::new(master, id, msg.clone().into(), &mut rng).unwrap();
-        let ct = circuit.resulted_ciphertext.clone();
-        let _proof = Groth16::prove(&pk, circuit, &mut rng).unwrap();
-    }
-
-    #[test]
-    fn test_nonnative_circuit() {
-        let mut rng = test_rng();
-        let bytes = [1, 2, 3];
-        let msg = ark_bls12_381::Fq::from_random_bytes(&bytes).unwrap();
-
-        let master: _ = {
-            let bytes = hex::decode("8200fc249deb0148eb918d6e213980c5d01acd7fc251900d9260136da3b54836ce125172399ddc69c4e3e11429b62c11").unwrap();
-            ark_bls12_381::G1Affine::deserialize_zk_crypto(&bytes).unwrap()
-        };
-        let round_number = 1000u64;
-        let id = {
-            let mut hash = sha2::Sha256::new();
-            hash.update(&round_number.to_be_bytes());
-            &hash.finalize().to_vec()[0..32]
-        };
-
-        let circuit = NonnativeCircuit::<ark_bls12_377::G1Projective>::new(
-            master.clone(),
-            &id,
-            msg.clone().into(),
-            &mut rng,
-        ).unwrap();
-
-        let (pk, _vk) = Groth16::<BW6_761>::setup(circuit, &mut rng).unwrap();
-
-        // let circuit = EncryptCircuit::new(master, id, msg.clone().into(), params.clone(), &mut rng).unwrap();
-        // let ct = circuit.resulted_ciphertext.clone();
-        // let _proof = Groth16::prove(&pk, circuit, &mut rng).unwrap();
     }
 }
